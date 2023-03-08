21b6ef02c1b6405158d3d4d8b1e388c6e809b023
Mikita Havryliuk <chinter.games1@gmail.com>
2023-03-08 20:46:33 +0200

    created dto

--- backend/src/comments/dto/create-comment.dto.ts
+++ backend/src/comments/dto/create-comment.dto.ts
@@ -1,1 +1,6 @@
-export class CreateCommentDto {}
+export class CreateCommentDto {
+  readonly userName: string;
+  readonly email: string;
+  readonly homePage?: string;
+  readonly text: string;
+}
