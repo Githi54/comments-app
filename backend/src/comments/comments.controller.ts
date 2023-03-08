68dd560c7a4ed04ef0521a787e0b113990ca092a
Mikita Havryliuk <chinter.games1@gmail.com>
2023-03-08 22:02:56 +0200

    created controller

--- backend/src/comments/comments.controller.ts
+++ backend/src/comments/comments.controller.ts
@@ -6,8 +6,13 @@ import {
   Patch,
   Param,
   Delete,
+  Req,
+  Res,
+  HttpException,
+  HttpStatus,
 } from '@nestjs/common';
 import { CommentsService } from './comments.service';
+import { Request, Response } from 'express';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
 
@@ -16,7 +21,14 @@ export class CommentsController {
   constructor(private readonly commentsService: CommentsService) {}
 
   @Post()
-  create(@Body() createCommentDto: CreateCommentDto) {
+  create(@Body() createCommentDto: CreateCommentDto, @Req() req: Request) {
+    const captcha = req.body.captcha;
+    const captchaText = req.session.captchaText;
+
+    if (captcha !== captchaText) {
+      throw new HttpException('Invalid captcha', HttpStatus.BAD_REQUEST);
+    }
+
     return this.commentsService.create(createCommentDto);
   }
 
@@ -38,5 +50,13 @@ export class CommentsController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.commentsService.remove(+id);
+  }
+
+  @Get('captcha')
+  getCaptcha(@Req() req: Request, @Res() res: Response) {
+    const captcha = this.commentsService.generateCaptcha();
+
+    req.session.captchaText = captcha.text;
+    res.type('svg').send(captcha.data);
   }
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
+
+declare module 'express' {
+  interface Request {
+    session: any;
+  }
+}
 
 async function bootstrap() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.use(
+    session({
+      secret: 'my-secret-key',
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
 
   await app.listen(PORT, () => console.log(`App start in PORT ${PORT}`));
 }
