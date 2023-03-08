6604962aa43e7a97cf5a3f713c2fda5c8747d9b5
Mikita Havryliuk <chinter.games1@gmail.com>
2023-03-08 13:36:04 +0200

    create users resource

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = process.env.PORT || 5000;
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  await app.listen(PORT, () => console.log(`App start in PORT ${PORT}`));
 }
+
 bootstrap();
